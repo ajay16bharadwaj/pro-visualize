6f153a235476738ecb88670bd42ec2cc28a440dc
bharadwaja <ajay.bharadwaj@cshs.org>
2024-10-17 10:57:10 -0700

    handling file moves

--- compute_functions.py
+++ compute_functions.py
@@ -6,7 +6,10 @@ import time
 from io import StringIO
 from requests.adapters import HTTPAdapter, Retry
 import re
-
+import streamlit as st
+import plotly.io as pio
+from io import BytesIO
+import base64
 
 
 
@@ -102,3 +105,44 @@ def _get_uniprot_ids(row):
         return pt
 
 
+# Function to export figures as HTML
+def export_figures_to_html(figures_dict):
+    """
+    Export the figures to an HTML file.
+    Args:
+        figures_dict (dict): A dictionary containing the figure names and their Plotly objects.
+    Returns:
+        str: HTML content of the figures.
+    """
+    html_content = "<html><head><title>Exported Figures</title></head><body>"
+    html_content += "<h1>Exported Figures from the Streamlit App</h1>"
+
+    for fig_name, fig in figures_dict.items():
+        html_content += f"<h2>{fig_name}</h2>"
+        html_content += pio.to_html(fig, include_plotlyjs='cdn', full_html=False) # type: ignore
+
+    html_content += "</body></html>"
+    
+    return html_content
+
+# Function to generate a downloadable HTML file
+def create_download_button(figures_dict, filename="exported_figures.html"):
+    """
+    Creates a download button for the exported figures.
+    Args:
+        figures_dict (dict): A dictionary containing the figure names and their Plotly objects.
+        filename (str): The filename for the exported HTML file.
+    """
+    # Generate the HTML content
+    html_content = export_figures_to_html(figures_dict)
+
+    # Convert the HTML content to bytes for download
+    b64 = base64.b64encode(html_content.encode()).decode()
+
+    # Create a download button
+    st.download_button(
+        label="Download all figures as HTML",
+        data=BytesIO(html_content.encode()),
+        file_name=filename,
+        mime="text/html"
+    )
